c0395cc6f62e5106ee57da9048224f0a578b3d6e
CarlHC91 <carlos.hc.91@gmail.com>
2023-06-05 21:40:00 +0200

    Secure token

--- plugin.video.picast/main.py
+++ plugin.video.picast/main.py
@@ -11,7 +11,7 @@ kod = kodi.Kodi()
 
 addon = xbmcaddon.Addon()
 server_host = addon.getSetting('server_host')
-api_key = addon.getSetting('api_key')
+token_api = addon.getSetting('token_api')
 
 
 def run():
@@ -31,7 +31,7 @@ def run():
 
 
 def list_directory(params):
-  url = server_host + '/directoryDetails/findAllByParent?api_key=%s' % (str(api_key))
+  url = server_host + '/directoryDetails/findAllByParent?token_api=%s' % (str(token_api))
 
   request_details = {
     'parent_directory': {
@@ -52,7 +52,7 @@ def list_directory(params):
   except Exception as e:
     kod.log(str(e))
 
-  url = server_host + '/archiveDetails/findAllByParent?api_key=%s' % (str(api_key))
+  url = server_host + '/archiveDetails/findAllByParent?token_api=%s' % (str(token_api))
 
   request_details = {
     'parent_directory': {
@@ -76,7 +76,7 @@ def list_directory(params):
 def play_archive(params):
   id_archive = params.get('id_archive')
   label = params.get('label')
-  path = server_host + '/archiveDetails/downloadOne?id_archive=%s&api_key=%s' % (str(id_archive), str(api_key))
+  path = server_host + '/archiveDetails/downloadOne?id_archive=%s&token_api=%s' % (str(id_archive), str(token_api))
 
   kod.playVideo(label = label, path = path)
 
